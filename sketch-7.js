7261f6a47cdbedda4507811483a1b13bafc2881a
briauna <31833842+briauna@users.noreply.github.com>
2017-09-30 17:44:19 -0700

    Create sketch-7.js

---
+++ sketch-7.js
@@ -0,0 +1,41 @@
+x= 35
+y =140
+while (x<width) {
+  stroke(0,255,255);
+  noFill();
+  rect(x,y,20,20);
+  x=x+82;
+}
+x= 30
+y =135
+while (x<width) {
+  stroke(0,255,255);
+  noFill();
+  rect(x,y,30,30);
+  x=x+82;
+}
+x= 25
+y =130
+while (x<width) {
+  stroke(0,255,255);
+  noFill();
+  rect(x,y,40,40);
+  x=x+82;
+}
+x= 20
+y =125
+while (x<width) {
+  stroke(0,255,255);
+  noFill();
+  rect(x,y,50,50);
+  x=x+82;
+}
+
+x= 15
+y =120
+while (x<width) {
+  stroke(0,255,255);
+  noFill();
+  rect(x,y,60,60);
+  x=x+82;
+}
